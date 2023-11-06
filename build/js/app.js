59ee097912b4939f042c0d1623cbfba85db69d0c
B1ack0ut1 <42845068+B1ack0ut1@users.noreply.github.com>
2023-11-07 02:38:49 +0800

    Completed contact and footer section, added dark mode

--- build/js/app.js
+++ build/js/app.js
@@ -1,10 +1,24 @@
 const initApp = () => {
   const hamburger = document.querySelector("#hamburger");
   const menu = document.querySelector("#menu");
+  const anchorLinks = document.querySelectorAll(".anchorLink");
+  const moonBtn = document.querySelector("#moon");
+  const body = document.querySelector("body");
 
   hamburger.addEventListener("click", () => {
     menu.classList.toggle("hidden");
     hamburger.classList.toggle("bg-white");
+  });
+
+  anchorLinks.forEach((anchorLink) => {
+    anchorLink.addEventListener("click", () => {
+      menu.classList.toggle("hidden");
+      hamburger.classList.toggle("bg-white");
+    });
+  });
+
+  moonBtn.addEventListener("click", () => {
+    body.classList.toggle("dark");
   });
 };
 
